212fc68eb87370c6ea57a5aac2cb35deb2dc93f2
Ashil John <willsquad.aj@gmail.com>
2018-04-09 15:57:24 +0530

    April 9 Evening

--- files/js/main.js
+++ files/js/main.js
@@ -3,4 +3,14 @@ $(document).ready(function(){
         $('#primary-menu').slideToggle();
         $(this).find('svg').toggleClass('fa-bars fa-times');
     });
+
+    $('.tablink').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('.tablink').removeClass('current');
+		$('.tabcontent').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
 });
